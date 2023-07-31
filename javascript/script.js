622d2c12de04910a656200c03d9edaf4b503503e
MemersonSt <emerson.cajape@tecsu.edu.ec>
2023-07-31 10:38:34 -0500

    commit -m "Casi terminados"

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+
+const nombre = document.getElementById('intro');
+const button = document.getElementById('BtonUp');
+const formulario = document.getElementById('form');
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+    if(nombre.value.length < 1){
+        alert('Por favor, ingresa tu nombre');
+    }
+});
+
+button.addEventListener('click', () => {
+    window.location.href = '../index2.html';
+} );
